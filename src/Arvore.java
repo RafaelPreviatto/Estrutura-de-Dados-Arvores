20b696977d0f81fb86c8f90af8901f58207d716b
Rafael Previatto <previattorafael@edu.unifil.br>
2025-05-20 21:55:12 -0300

    Busca Pre Ordem e Em Ordem

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,4 +1,26 @@
 public class Arvore {
 
     No raiz;
+
+    public int contarNos (No node){
+        if (node == null) return 0;
+        return 1 + contarNos(node.esquerda) + contarNos(node.direita);
+    }
+
+    public void buscarPreOrdem(No node){
+        if (node != null){
+            System.out.println(node.valor + " ");
+            buscarPreOrdem(node.esquerda);
+            buscarPreOrdem(node.direita);
+        }
+    }
+
+    public void buscarEmOrdem(No node){
+        if (node != null){
+            buscarEmOrdem(node.esquerda);
+            System.out.println(node.valor + " ");
+            buscarEmOrdem(node.direita);
+        }
+    }
+
 }
