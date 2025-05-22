c8ff7cc940cf7fe2ae4b83097ae1e7c127188f27
Rafael Previatto <previattorafael@edu.unifil.br>
2025-05-22 20:14:53 -0300

    Pós Ordem Em Nível

--- src/Arvore.java
+++ src/Arvore.java
@@ -34,22 +34,6 @@ public class Arvore {
         }
     }
 
-    public void buscarEmNivel( No node){
-        if (raiz == null)
-            return;
-
-        Queue<No> fila = new LinkedList<>();
-        fila.add(raiz);
-
-        while (!fila.isEmpty()) {
-            No atual = fila.poll();
-            System.out.println(atual.valor + " ");
-
-            if (atual.esquerda != null) fila.add(atual.esquerda);
-            if (atual.direita != null) fila.add(atual.direita);
-        }
-    }
-
 
 
 
