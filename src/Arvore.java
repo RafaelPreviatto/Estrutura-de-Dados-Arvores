0497be6d944a8daef14c07fa1ed790c7e20a78dd
Rafael Previatto <previattorafael@edu.unifil.br>
2025-05-27 21:50:04 -0300

    Implementação Interativa

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,5 +1,6 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
 public class Arvore {
 
@@ -34,22 +35,83 @@ public class Arvore {
         }
     }
 
-    public void searchInLevel(No node) {
-        if (node != null) {
-            Queue<No> fila = new LinkedList<>();
-            fila.add(node);
+    public void buscaEmNivel(No node) {
+        if (node != null) return;
 
-            while (!fila.isEmpty()) {
-                No atual = fila.poll();
-                System.out.println(atual.valor + "  ");
+        Queue<No> fila = new LinkedList<>();
+        fila.add(node);
 
-                if (atual.esquerda != null) {
-                    fila.add(atual.esquerda);
-                }
-                if (atual.direita != null) {
-                    fila.add(atual.direita);
-                }
+        while (!fila.isEmpty()) {
+            No atual = fila.poll();
+            System.out.println(atual.valor + "  ");
+
+            if (atual.esquerda != null) {
+                fila.add(atual.esquerda);
+            }
+            if (atual.direita != null) {
+                fila.add(atual.direita);
             }
         }
     }
+
+    public void preOrdemInterativo(No raiz) {
+        if (raiz == null) return;
+
+        Stack<No> pilha = new Stack<>();
+        pilha.push(raiz);
+
+        while (!pilha.isEmpty()) {
+            No atual = pilha.pop();
+            System.out.println(atual.valor + " ");
+
+            if (atual.direita != null) pilha.push(atual.direita);
+            if (atual.esquerda != null) pilha.push(atual.esquerda);
+        }
+    }
+
+    public void emOrdemInterativo(No raiz){
+        Stack<No> pilha = new Stack<>();
+        No atual = raiz;
+
+        while (atual != null || !pilha.isEmpty()){
+            while (atual != null){
+                pilha.push(atual);
+                atual = atual.esquerda;
+            }
+
+            atual = pilha.pop();
+            System.out.println(atual.valor);
+
+            atual = atual.direita;
+        }
+    }
+
+    public void posOrdemInterativo(No raiz) {
+        if (raiz == null) return;
+
+        Stack<No> pilha1 = new Stack<>();
+        Stack<No> pilha2 = new Stack<>();
+
+        pilha1.push(raiz);
+
+        while (!pilha1.isEmpty()) {
+            No atual = pilha1.pop();
+            pilha2.push(atual);
+
+            if (atual.esquerda != null) {
+                pilha1.push(atual.esquerda);
+            }
+
+            if (atual.direita != null) {
+                pilha1.push(atual.direita);
+            }
+        }
+
+        while (!pilha2.isEmpty()) {
+            No atual = pilha2.pop();
+            System.out.println(atual.valor);
+        }
+    }
+
+
 }
