9b03ac99d44dc8bde05a99d51b08fdf5d82b4f2b
Rafael Previatto <previattorafael@edu.unifil.br>
2025-05-29 20:14:43 -0300

    Balanceamento

--- src/No.java
+++ src/No.java
@@ -2,10 +2,21 @@ public class No {
 
     String valor;
     No esquerda, direita;
+    int altura;
+    int fatorBalanceamento;
 
-    public No (String valor){
+    public No(String valor) {
         this.valor = valor;
         esquerda = direita = null;
+        altura = 1;
+        fatorBalanceamento = 0;
     }
 
+    public void atualizarAlturaEFator() {
+        int alturaEsquerda = (esquerda != null) ? esquerda.altura : 0;
+        int alturaDireita = (direita != null) ? direita.altura : 0;
+
+        altura = 1 + Math.max(alturaEsquerda, alturaDireita);
+        fatorBalanceamento = alturaEsquerda - alturaDireita;
+    }
 }
